795a293338a2445a64b068e2ef02bbb21ec62c7e
JomariMiguel-Carlos <joamrimadla0@gmail.com>
2023-01-23 12:49:12 +0800

    done bubble sort, so the idea of bubble sort is to compare values 1 by 1 until you achieve the final form will discuss more on the video tutorial

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,1 +1,13 @@
+def sort(nums):
+    for i in range(len(nums)-1,0,-1):
+        for j in range(i):
+            if nums[j]>nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+
+
 nums = [59,92,93,71,88,26,14,8,81,11]
+sort(nums)
+
+print(nums)
