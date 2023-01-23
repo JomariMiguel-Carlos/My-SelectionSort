529c62e658ff7373b2d28e46a5e850c0b5b934c4
JomariMiguel-Carlos <joamrimadla0@gmail.com>
2023-01-23 11:08:15 +0800

    done selection sort and it is same with my manual slection sort

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,7 +1,22 @@
+print('nums = [59,92,93,71,88,26,14,8,81,11]')
+print("")
+
 def sort (nums):
 
     for i in range(9):
         minpos = i
-        for j in range(i,10)
+        for j in range(i,10):
+            if nums[j] < nums[minpos]:
+                minpos = j
+
+
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+
+        print(nums)
 
 nums = [59,92,93,71,88,26,14,8,81,11]
+sort(nums)
+
+print(nums)
